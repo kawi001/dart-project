173b2dd339d3cfed03e7eec0728f5ccb552d6f1b
kawi001 <kawippp@gmail.com>
2024-11-18 16:31:59 +0700

    .

---
+++ reminder.dart
@@ -0,0 +1,8 @@
+void main() {
+ int dividend = 10;
+ int divisor = 3;
+ int quotient = dividend ~/ divisor;
+ int remainder = dividend % divisor;
+ print('quotient = $quotient');
+ print('remainder = $remainder');
+}
